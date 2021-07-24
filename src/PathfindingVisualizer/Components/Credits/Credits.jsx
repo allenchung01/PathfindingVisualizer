7e82c945ca192509de83f7e28cff2135f8c53d9e
allenchung <allenchung01@gmail.com>
2021-07-24 01:14:58 -0500

    Added a credits component

---
+++ src/PathfindingVisualizer/Components/Credits/Credits.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+
+export default function Credits() {
+  return (
+    <div className="credits">
+      <a target="_blank" href="https://icons8.com/icon/62234/launch">
+        Launch
+      </a>{" "}
+      icon by{" "}
+      <a target="_blank" href="https://icons8.com">
+        Icons8
+      </a>
+      <a target="_blank" href="https://icons8.com/icon/100900/empty-flag">
+        Empty Flag
+      </a>{" "}
+      icon by{" "}
+      <a target="_blank" href="https://icons8.com">
+        Icons8
+      </a>
+      <a target="_blank" href="https://icons8.com/icon/118838/vertical-line">
+        Vertical Line
+      </a>{" "}
+      icon by{" "}
+      <a target="_blank" href="https://icons8.com">
+        Icons8
+      </a>
+      <div>
+        Icons made by{" "}
+        <a href="https://www.freepik.com" title="Freepik">
+          Freepik
+        </a>{" "}
+        from{" "}
+        <a href="https://www.flaticon.com/" title="Flaticon">
+          www.flaticon.com
+        </a>
+      </div>
+      <div>
+        Icons made by{" "}
+        <a href="" title="Vitaly Gorbachev">
+          Vitaly Gorbachev
+        </a>{" "}
+        from{" "}
+        <a href="https://www.flaticon.com/" title="Flaticon">
+          www.flaticon.com
+        </a>
+      </div>
+    </div>
+  );
+}
--- src/PathfindingVisualizer/PathfindingVisualizer.jsx
+++ src/PathfindingVisualizer/PathfindingVisualizer.jsx
@@ -7,6 +7,7 @@ import NavigationBar from "./NavigationBar/NavigationBar";
 import DrawToggle from "./Buttons/DrawToggle/DrawToggle";
 import Slider from "./Buttons/Slider/Slider";
 import AlgorithmTitle from "./AlgorithmTitle/AlgorithmTitle";
+import Credits from "./Components/Credits/Credits";
 
 // Functions
 import { dijkstra } from "./Search Algorithms/Dijkstra";
@@ -123,43 +124,7 @@ export default class PathfindingVisualizer extends Component {
         </NavigationBar>
 
         {this.displayGrid(grid)}
-        {/*<div className="credits">
-          <a target="_blank" href="https://icons8.com/icon/62234/launch">
-            Launch
-          </a>{" "}
-          icon by{" "}
-          <a target="_blank" href="https://icons8.com">
-            Icons8
-          </a>
-          <a target="_blank" href="https://icons8.com/icon/100900/empty-flag">
-            Empty Flag
-          </a>{" "}
-          icon by{" "}
-          <a target="_blank" href="https://icons8.com">
-            Icons8
-          </a>
-          <a
-            target="_blank"
-            href="https://icons8.com/icon/118838/vertical-line"
-          >
-            Vertical Line
-          </a>{" "}
-          icon by{" "}
-          <a target="_blank" href="https://icons8.com">
-            Icons8
-          </a>
-          <div>
-            Icons made by{" "}
-            <a href="https://www.freepik.com" title="Freepik">
-              Freepik
-            </a>{" "}
-            from{" "}
-            <a href="https://www.flaticon.com/" title="Flaticon">
-              www.flaticon.com
-            </a>
-          </div>
-    </div>
-    <div>Icons made by <a href="" title="Vitaly Gorbachev">Vitaly Gorbachev</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>*/}
+        <Credits />
       </div>
     );
   }
