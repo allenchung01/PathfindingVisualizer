936f5d76102dcf9fcc537d1b474c73e7ed18770a
allenchung <allenchung01@gmail.com>
2021-08-06 16:19:24 -0500

    finished simple implementation of tutorial.

--- src/PathfindingVisualizer/Components/Tutorial/Tutorial.jsx
+++ src/PathfindingVisualizer/Components/Tutorial/Tutorial.jsx
@@ -10,21 +10,49 @@ export default function Tutorial(props) {
     {
       title: "Welcome!",
       description:
-        "This is my pathfinding visualizer! Click continue to view the tutorial.",
+        "This is my pathfinding visualizer. Select an algorithm, draw some walls or weights, and watch as the rocket finds a path to the flag.",
     },
-    { title: "Title 2", description: "Description 2" },
-    { title: "Title 3", description: "Description 3" },
+    {
+      title: "Rules",
+      description:
+        "The rocket may only move into adjacent, non-diagonal squares. Moves are a distance of 1 unless occupied by a weight with a specified distance. The rocket may not move through walls.",
+    },
+    {
+      title: "Selecting an Algorithm",
+      description:
+        "Use the dropdown in the upper left corner of the screen to select an algorithm. Hover over the info icon in the lower left of the screen to view more information about the algorithm. Click on 'Go' when you are ready visualize.",
+    },
+    {
+      title: "Drawing Walls and Weights",
+      description:
+        "Use the 'Draw Mode' toggle at the top of the screen to switch between wall mode and weight mode. Drag across the squares on the screen to draw using the selected mode. Walls, paths, and weights, can be cleared using the buttons to the left of the 'Draw Mode' toggle.",
+    },
+    {
+      title: "Adjusting Weight Values",
+      description:
+        "Use the 'Weight Value' slider at the top of the screen to adjust the distance it will take for the rocket to pass through weights. Not all algorithms take weights into account.",
+    },
+    {
+      title: "Moving Start and End Nodes.",
+      description:
+        "The rocket and flag can be moved by dragging them across the grid. Dragging either of these nodes after a path has already been discovered will auto update the path to reflect the new positions.",
+    },
   ];
 
   function nextPage() {
     // Increment page until last page.
     if (page + 1 < pages.length) {
       setPage(page + 1);
+    } else {
+      exitTutorial();
     }
   }
 
   function prevPage() {
     // Decrement page until first page.
+    if (page - 1 >= 0) {
+      setPage(page - 1);
+    }
   }
 
   return (
@@ -32,11 +60,15 @@ export default function Tutorial(props) {
       <h2 id="tutorial-title">{pages[page].title}</h2>
       <p id="tutorial-description">{pages[page].description}</p>
       <div id="tutorial-buttons">
-        <button id="skip-button" onClick={exitTutorial}>
+        <button id="skip-button" onClick={page == 0 ? exitTutorial : prevPage}>
           {page == 0 ? "Skip Tutorial" : "Previous"}
         </button>
         <button id="continue-button" onClick={nextPage}>
-          {page == 0 ? "Continue" : "Next"}
+          {page == 0
+            ? "Continue"
+            : page == pages.length - 1
+            ? "End Tutorial"
+            : "Next"}
         </button>
       </div>
     </div>
